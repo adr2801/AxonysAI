293bc97ace14dd42c463ba5b5942f19e29785a1f
adr2801 <153359378+adr2801@users.noreply.github.com>
2026-03-22 12:00:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import IA_base
 
 def main(page: ft.Page):
     page.title = "Cortex IA"
+    page.assets_dir = "icon.png"
     page.theme_mode = ft.ThemeMode.LIGHT
     page.scroll = ft.ScrollMode.ADAPTIVE
 
@@ -26,7 +27,7 @@ def main(page: ft.Page):
     slider_env = ft.Slider(min=0, max=10, divisions=10, label="{value}")
     slider_ene = ft.Slider(min=0, max=10, divisions=10, label="{value}")
 
-    
+
 
 
     tableau = ft.DataTable(
@@ -34,7 +35,7 @@ def main(page: ft.Page):
             ft.DataColumn(ft.Text("Tâche")),
             ft.DataColumn(ft.Text("Score (%)"),numeric=True),
         ],
-        rows=[] 
+        rows=[]
     )
     def calculer_priorite(e):
         chargement.visible = True
@@ -45,14 +46,12 @@ def main(page: ft.Page):
         tableau.rows.append(ft.DataRow(cells=[ft.DataCell(ft.Text(nom_tache.value)), ft.DataCell(ft.Text(f"{score*100:.1f}"))]))
         tableau.rows.sort(key=lambda row: float(row.cells[1].content.value.replace('%', '')), reverse=True)
         chargement.visible = False
-        
+
         texte_statut.value = "Priorité calculée"
-        
+
         nom_tache.value = ""
         page.update()
     page.add(
-        chargement,
-        texte_statut,
         ft.Text("Mon assistant Priorité", size=30, weight=ft.FontWeight.BOLD),
         nom_tache,
         ft.Text("Importance"),
@@ -67,8 +66,10 @@ def main(page: ft.Page):
         slider_ene,
         ft.Button("Ajouter et trier", on_click=calculer_priorite),
         ft.Divider(),
+        chargement,
+        texte_statut,
         ft.Text("Tableau des tâches prioritaires", size=20, weight=ft.FontWeight.BOLD),
         ft.Column([tableau], scroll=ft.ScrollMode.ADAPTIVE, height=400)
     )
 
-ft.run(main)
+ft.run(main, )
